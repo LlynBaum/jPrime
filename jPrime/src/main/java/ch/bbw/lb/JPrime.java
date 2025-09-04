6bdaf13186f8c6112d7ea49fab71dde991132c81
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-09-04 12:07:54 +0200

    implement is Prime

--- jPrime/src/main/java/ch/bbw/lb/JPrime.java
+++ jPrime/src/main/java/ch/bbw/lb/JPrime.java
@@ -1,4 +1,19 @@
 package ch.bbw.lb;
 
 public class JPrime {
+    public boolean isPrime(int i) {
+        if (i <= 1) {
+            return false;
+        }
+        for (int n = 2; n <= Math.sqrt(i); n++) {
+            if (i % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isNotPrime(int i) {
+        return !isPrime(i);
+    }
 }
